67d5e61f325883b699008d85e981e83ffd6e2e08
1900295 <1900295@uad.ac.uk>
2020-01-23 13:10:40 +0000

    Initialised font, text and cirlce objects and drag task variables. In the handleInput() function made various things happen due to keyboard presses. Showed position of mouse in window. Prints distance of drag with left mouse button to console. Set the position of circle to mouse position when right mouse button is pressed. Rendered text and circle in window.

--- Week2/CMP105App/Level.cpp
+++ Week2/CMP105App/Level.cpp
@@ -6,7 +6,25 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 	input = in;
 
 	// initialise game objects
+	if (!font.loadFromFile("font/arial.ttf"))
+	{
+		std::cout << "Error Loading Font" << std::endl;
+	}
 
+	text.setFont(font);
+	text.setString("");
+	text.setCharacterSize(24);
+	text.setFillColor(sf::Color::Red);
+
+	circle.setRadius(50);
+	circle.setPosition(50,50);
+	circle.setFillColor(sf::Color::Red);
+
+	drag = false;
+	startDragX = 0;
+	startDragY = 0;
+	endDragX = 0;
+	endDragY = 0;
 }
 
 Level::~Level()
@@ -17,8 +35,62 @@ Level::~Level()
 // handle user input
 void Level::handleInput()
 {
+	if (input->isKeyDown(sf::Keyboard::W))
+	{
+		std::cout << "Nailed it" << std::endl;
+	}
+
+	if (input->isKeyDown(sf::Keyboard::J) && input->isKeyDown(sf::Keyboard::K) && input->isKeyDown(sf::Keyboard::L))
+	{
+		std::cout << "Really nailed it" << std::endl;
+	}
+
+	if (input->isKeyDown(sf::Keyboard::Escape))
+	{
+		window->close();
+	}
+
+	std::string output = "Mouse X: ";
+	output.append(std::to_string(input->getMouseX()));
+	output.append("  Mouse Y: ");
+	output.append(std::to_string(input->getMouseY()));
+	text.setString(output);
 
 
+	if (input->isMouseLDown() && drag == false)
+	{
+		startDragX = input->getMouseX();
+		startDragY = input->getMouseY();
+		drag = true;
+	}
+	if (!input->isMouseLDown() && drag == true)
+	{
+		endDragX = input->getMouseX();
+		endDragY = input->getMouseY();
+		int diffX = startDragX - endDragX;
+		int diffY = startDragY - endDragY;
+		if (diffX < 0)
+		{
+			diffX *= -1;
+		}
+		if (diffY < 0)
+		{
+			diffY *= -1;
+		}
+		int distance = (diffX ^ 2 + diffY ^ 2) ^ (1/2);
+		std::cout << "You dragged a distance of: " << distance << std::endl;
+
+		drag = false;
+		startDragX = 0;
+		startDragY = 0;
+		endDragX = 0;
+		endDragY = 0;
+	}
+
+	if (input->isMouseRDown())
+	{
+		circle.setPosition(input->getMouseX(), input->getMouseY());
+	}
 }
 
 // Update game objects
@@ -31,6 +103,9 @@ void Level::update()
 void Level::render()
 {
 	beginDraw();
+
+	window->draw(text);
+	window->draw(circle);
 
 	endDraw();
 }
